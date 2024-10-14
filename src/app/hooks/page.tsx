5bad6e6fa51ac7c8a80338b3d45bd5bf8266e1c2
Lê Nhữ Bắc <95062907+abcmonboi@users.noreply.github.com>
2024-10-14 17:36:27 +0700

    Update page.tsx

--- src/app/hooks/page.tsx
+++ src/app/hooks/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const HooksComponent = () => {
   return (
-    <div>HooksComponent</div>
-  )
-}
+    <div className="p-8">
+      {" "}
+      <h1 className="text-4xl font-bold mb-6 text-blue-700">
+        Welcome to the Beach Dashboard 🏖️
+      </h1>
+      <p className="text-lg text-gray-600">
+        This is your main content area where you can display your data or other
+        information.
+      </p>
+    </div>
+  );
+};
 
-export default HooksComponent
+export default HooksComponent;
