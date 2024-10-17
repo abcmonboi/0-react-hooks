0760b03249cc380649c466cb41237f98be34903b
Lê Nhữ Bắc <95062907+abcmonboi@users.noreply.github.com>
2024-10-17 16:46:11 +0700

    Update page.tsx

--- src/app/hooks/mounted/page.tsx
+++ src/app/hooks/mounted/page.tsx
@@ -1,47 +1,62 @@
+import { cn } from "@/lib/utils";
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-const page = () => {
+const notifications = [
+  {
+    title: "Gán và Gỡ",
+    required: false,
+  },
+  {
+    title:
+      "Mounted là khi component được đưa vào React.Element trước khi vào Dom",
+  },
+  {
+    title:
+      "UnMounted là khi component được gỡ khỏi Dom và trước khi rời React.Element",
+  },
+  {
+    title: "Viết Biểu thức điều kiện để 1 component unmount sử dụng useEffect",
+  },
+];
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+const page = ({ className, ...props }: CardProps) => {
   return (
-    <div className="flex flex-col gap-7 px-28">
-      <h1 className="text-5xl font-bold text-customColors-slate text-center">
-        Mounted/UnMounted
-      </h1>
-      <div className="text-gray-700 dark:text-gray-300 text-2xl space-y-4">
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>Gán và Gỡ </span>
+    <Card className={cn("", className)} {...props}>
+      <CardHeader>
+        <CardTitle className="text-center text-5xl text-customColors-oceanBlue ">{`Mounted/UnMounted`}</CardTitle>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div>
+          {notifications.map((item, index) => (
+            <div
+              key={index}
+              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+            >
+              <span
+                className={`flex text-4xl h-2 w-2 translate-y-1 rounded-full ${
+                  item?.required ? `text-red-500` : ` text-sky-500 `
+                }`}
+              >
+                {item?.required ? `*` : `•`}
+              </span>
+              <div className="py-3 pl-2">
+                <p className="text-lg font-medium leading-none">{item.title}</p>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>
-            Mounted là khi component được đưa vào React.Element trước khi vào
-            Dom
-          </span>
-        </div>{" "}
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>
-            UnMounted là khi component được gỡ khỏi Dom và trước khi rời
-            React.Element
-          </span>
-        </div>{" "}
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>
-            Viết Biểu thức điều kiện để 1 component unmount sử dụng useEffect
-          </span>
-        </div>{" "}
-      </div>
-
-      <div className="flex sjustify-center items-center space-x-4">
-        <button className="px-6 py-3 rounded-full bg-blue-500 text-white hover:bg-customColors-lightBlue shadow-md transition-all duration-300">
-          Get Started
-        </button>
-        <button className="px-6 py-3 rounded-full bg-blue-100 text-blue-500 hover:bg-blue-200 shadow-md transition-all duration-300">
-          Learn More
-        </button>
-      </div>
-    </div>
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
   );
 };
 
