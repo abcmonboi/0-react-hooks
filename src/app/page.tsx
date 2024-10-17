5949de239964fb088220c9c3e100efaf3ab10e76
Lê Nhữ Bắc <95062907+abcmonboi@users.noreply.github.com>
2024-10-17 16:45:47 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,71 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-5xl font-bold text-customColors-slate text-center">
-        Welcome to React Hooks 2022
-      </h1>
-      <div className="text-gray-700 dark:text-gray-300 text-2xl space-y-4">
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>Hooks có nghĩa là gắn vào, móc vào</span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">•</span>
-          <span>
-            Hooks là tên khái niệm - là một hàm được viết sẵn sử dụng cho 1 sự
-            việc, hành động cụ thể nhiều lần ở nhiều nơi
-          </span>
-        </div>{" "}
-        <div className="flex items-center space-x-2">
-          <span className="text-red-500">*</span>
-          <span className="text-red-500">Chỉ dùng cho Function component</span>
-        </div>
-        <div className="flex items-center space-x-2">
-          <span className="text-blue-500">{`=>`}</span>
-          <span>Hãy sử dụng Hooks khi tạo mới dự án React</span>
-        </div>
-      </div>
+import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-      <div className="flex justify-center items-center space-x-4">
-        <button className="px-6 py-3 rounded-full bg-blue-500 text-white hover:bg-customColors-lightBlue shadow-md transition-all duration-300">
-          Get Started
-        </button>
-        <button className="px-6 py-3 rounded-full bg-blue-100 text-blue-500 hover:bg-blue-200 shadow-md transition-all duration-300">
-          Learn More
-        </button>
-      </div>
-    </>
+const notifications = [
+  {
+    title: "Hooks có nghĩa là gán vào, móc vào",
+  },
+  {
+    title: "Sử dụng Hooks khi tạo mới dự án React",
+  },
+  {
+    title: "Chỉ dùng cho Function component",
+    required: true,
+  },
+];
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+export default function Home({ className, ...props }: CardProps) {
+  return (
+    <Card className={cn("", className)} {...props}>
+      <CardHeader>
+        <CardTitle className="text-center text-5xl text-customColors-oceanBlue ">{`Một số kiến thức phải nhớ về React 2022`}</CardTitle>
+        <CardDescription className="text-xl">
+          Website này cung cấp cho bạn một số kiến thức về React. Đặc biệt là
+          <b> Hooks</b>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className=" flex items-center space-x-4 rounded-md border p-4">
+          <div className="flex-1 space-y-1">
+            <p className="text-lg font-medium leading-none">
+              Một vài điều cơ bản có thể bạn đã biết về hooks
+            </p>
+            <p className="text-lg text-muted-foreground">
+              Hooks là tên khái niệm - là một hàm được viết sẵn sử dụng cho 1 sự
+              việc, hành động cụ thể nhiều lần ở nhiều nơi
+            </p>
+          </div>
+        </div>
+        <div>
+          {notifications.map((item, index) => (
+            <div
+              key={index}
+              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+            >
+              <span
+                className={`flex text-4xl h-2 w-2 translate-y-1 rounded-full ${
+                  item?.required ? `text-red-500` : ` text-sky-500 `
+                }`}
+              >
+                {item?.required ? `*` : `•`}
+              </span>
+              <div className="py-3 pl-2">
+                <p className="text-lg font-medium leading-none">{item.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter></CardFooter>
+    </Card>
   );
 }
