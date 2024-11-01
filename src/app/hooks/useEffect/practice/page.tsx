c1643a8a8e05dcfb39f78cdaeb717fa6da00456f
ABCoder <gibb6699@gmail.com>
2024-11-01 12:16:25 +0700

    fix useEffect

--- src/app/hooks/useEffect/practice/page.tsx
+++ src/app/hooks/useEffect/practice/page.tsx
@@ -10,10 +10,20 @@ import {
 import React, { useEffect, useState } from "react";
 
 const PracticeUseEffect = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="grid gap-7 min-h-[120vh]">
       <UseEffectWithDomEvents />
       <UseEffectWithDomEventsResize />
+      <Button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="text-center text-3xl py-10"
+        size={"lg"}
+      >
+        Toggle Timer Countdown
+      </Button>
+      {isOpen && <UseEffectWithTimerFunctions />}
     </div>
   );
 };
@@ -34,7 +44,7 @@ function UseEffectWithDomEvents() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>3.Listen Dom Event - Scroll Practice</CardTitle>
+        <CardTitle>4.Listen Dom Event - Scroll Practice</CardTitle>
       </CardHeader>
       <CardContent>
         {" "}
@@ -93,7 +103,7 @@ function UseEffectWithDomEventsResize() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>3.Listen Dom Event - Resize Practice</CardTitle>
+        <CardTitle>4.Listen Dom Event - Resize Practice</CardTitle>
       </CardHeader>
       <CardContent>
         <Button className="w-full px-9 py-8 text-2xl">{currentWidth}</Button>
@@ -101,4 +111,58 @@ function UseEffectWithDomEventsResize() {
     </Card>
   );
 }
+
+function UseEffectWithTimerFunctions() {
+  const [countdown, setCountDown] = useState(180);
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCountDown((prev) => {
+        return prev - 1;
+      });
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, []);
+
+  // setTimeout(() => {
+  //   setCountDown((prev) => {
+  //     return prev - 1;
+  //   });
+  // }, 1000);
+
+  return (
+    <Card className="translate-y-4 slide-in-from-top-2/3  ease-in-out transform-gpu transition-all">
+      <CardHeader>
+        <CardTitle>5. Use Effect with timers functions</CardTitle>
+        <CardDescription className="text-red-500">
+          ***Kiến thức cơ bản: setInterval, setTimeout, clearInterval,
+          clearTimeout
+        </CardDescription>
+        <CardDescription>
+          Hiểu được ý nghĩa và khi dùng (Closure)
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>BT. Đếm ngược (vẫn dùng state)</CardDescription>
+        <CardDescription className="text-red-500">
+          ***Nếu không đặt setInterval vào trong useEffect thì cũng sẽ bị như
+          trường hợp gọi API đó là logic chạy trước khi component render UI
+          (state đã set lại) --- Khi render UI xong thì state đã bị set lại ----
+          Re-render --- chạy lại logic set state liên tục
+          <br />
+          --Khi dùng useEffect với các functions hàm đóng(closure). Hàm chỉ tạo
+          1 lần thì không nên sử dụng các biến ngoài phạm vi. ví dụ:
+          setState(state +1) ===vvv setState(prev prev+1)
+        </CardDescription>
+
+        <div className="flex gap-5  justify-center items-center ">
+          <Button className="size-20 text-center text-3xl mt-20">
+            {countdown}
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
 export default PracticeUseEffect;
