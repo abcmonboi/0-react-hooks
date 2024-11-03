9440957a6f54bb093211f9c1efc1dd676663f6ab
ABCoder <gibb6699@gmail.com>
2024-11-03 22:27:48 +0700

    useRef

--- src/app/hooks/use-ref/page.tsx
+++ src/app/hooks/use-ref/page.tsx
@@ -1,7 +1,62 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const page = () => {
-  return <div>Knowledge</div>;
+  return (
+    <div className="grid gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>useRef - Mục đích sử dụng</CardTitle>
+          <CardDescription>
+            Lưu giá trị tham chiếu ra bên ngoài 1 function component
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <li id="ss" className="marker:text-red-500">
+            Mỗi component tạo sẽ sinh ra phạm vi mới không liên quan đến phạm vi
+            trước đó.
+          </li>
+          <li id="sss" className="marker:text-red-500">
+            Biến tạo bằng <b>let</b> ở phạm vi component render lúc đầu sẽ khác
+            biến tạo ở component sau khi được re-render
+          </li>
+          <li className="marker:text-red-500">
+            Dùng useRef để giữ giá trị mới mỗi lần compoonent, function tạo lại
+            (re-render) và được tạo bằng const
+          </li>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Cách dùng </CardTitle>
+          <CardDescription>
+            Khởi tạo bằng const varName = useRef(initValue)
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <li id="ss" className="marker:text-red-500">
+            initValue là giá trị khởi tạo, chỉ sử dụng trong 1 lần đầu tiên, nếu
+            re-render giá trị sẽ không sử dụng hay tạo lại nữa
+          </li>
+          <li id="sss" className="marker:text-red-500">
+            Hàm trả về 1 object có property current. Current dùng để gán giá trị{" "}
+            <code>ref.current = value</code>
+          </li>
+          <li className="marker:text-red-500">
+            Khi gán giá trị bằng current thì ref sẽ không reset lại giá trị ban
+            đầu khởi tạo mà sẽ luôn nhận giá trị mới
+          </li>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
--- src/app/hooks/use-ref/practice/page.tsx
+++ src/app/hooks/use-ref/practice/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React, { useRef, useState } from "react";
 
 const DemoUseRef = () => {
-  return <div>DemoUseRef</div>;
+  const [countdown, setCountdown] = useState(80);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const handleCountDown = () => {
+    if (timerRef.current) return;
+    timerRef.current = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    if (!timerRef.current) return;
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+    buttonRef.current?.disabled == false;
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Demo UseRef using timer countdown</CardTitle>
+        <CardDescription>
+          Problem to use: Using let varName = timerID not save ref of var.
+        </CardDescription>
+        <CardContent className="text-center">
+          <li className="marker:text-red-500">
+            SetRef không render lại component
+          </li>
+          <h5 className="inline">Count</h5>
+          <h5 className="inline"> {countdown}</h5>
+          <div className="mt-4">
+            <Button
+              ref={buttonRef}
+              //   disabled={!!timerRef.current}
+              onClick={handleCountDown}
+              className="mr-8"
+            >
+              Start
+            </Button>
+            <Button disabled={!timerRef.current} onClick={handleStop}>
+              Stop
+            </Button>
+          </div>
+        </CardContent>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default DemoUseRef;
